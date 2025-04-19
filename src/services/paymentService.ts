5a6fba902d4f99feca558462d998761dfa62fc19
Nelson Cheung <cubecnelson@gmail.com>
2025-04-19 21:09:37 +0800

    fix: fix create payment intent error

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -18,6 +18,7 @@ export const initializePayment = async (userId: string, userEmail: string, tierI
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+            userEmail,
             userId,
             tierId,
             tier,
